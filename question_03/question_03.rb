dc0d293579cfda9251fba9f3cd294cdf90a4017e
mariaperhaps <mariatschettino@gmail.com>
2016-02-18 17:20:16 -0500

    deleting comment

--- question_03/question_03.rb
+++ question_03/question_03.rb
@@ -13,20 +13,6 @@ def is_prime?(number)
     return true
 end
 
-# def is_prime?(number)
-#     temp = []
-#         (2..Math.sqrt(number)).each do |factor|
-#           if number % factor == 0 && number / factor != 1
-#             temp.push(number)
-#           end
-#         end
-#     if temp.length == 0
-#       return true
-#     else
-#       return false
-#     end
-
-# end
 
 def get_largest_prime_factor(number)
   largest =  0
